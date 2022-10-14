4f97abfa23041d1d1967b7ecc3ddfcfc2a1c4816
flipfrog <>
2022-10-14 10:12:02 +0900

    modify for parallel fetching and using mutex.

--- exercise.go
+++ exercise.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
 
 type Fetcher interface {
 	// Fetch returns the body of URL and
@@ -12,7 +19,7 @@ type Fetcher interface {
 
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
+func Crawl(url string, depth int, fetcher Fetcher, c *SafeCounter) {
 	// TODO: Fetch URLs in parallel.
 	// TODO: Don't fetch the same URL twice.
 	// This implementation doesn't do either:
@@ -24,15 +31,33 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+	c.mu.Lock()
+	if c.v[url] > 0 {
+		c.mu.Unlock()
+		return
 	}
+	c.v[url]++
+	c.mu.Unlock()
+	fmt.Printf("found: %s %q\n", url, body)
+
+	var wg sync.WaitGroup
+	for _, u := range urls {
+		wg.Add(1)
+		go func(u string) {
+			defer wg.Done()
+			Crawl(u, depth-1, fetcher, c)
+		}(u)
+	}
+	wg.Wait()
 	return
 }
 
 func main() {
-	Crawl("https://golang.org/", 4, fetcher)
+	c := SafeCounter{v: make(map[string]int)}
+	Crawl("https://golang.org/", 4, fetcher, &c)
+	for url, _ := range c.v {
+		fmt.Println("*** ", url)
+	}
 }
 
 // fakeFetcher is Fetcher that returns canned results.
